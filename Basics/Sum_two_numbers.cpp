9ae78889093781d80a9748afea0dbcd153c1ce3c
Shrey Thakar <shreythakar05@gmail.com>
2026-01-04 20:57:04 +0530

    Add Sum_two_numbers.cpp

---
+++ Basics/Sum_two_numbers.cpp
@@ -0,0 +1,18 @@
+// This C++ code is to take two numbers from the user and print their sum.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N1;
+    int N2;
+    int sum;
+
+    cin >> N1;
+    cin >> N2;
+    sum = N1 + N2;
+
+    cout << sum;
+
+    return 0;
+}
