67805d9c5e72575974ab34451797969b7f3fe4cf
Shrey Thakar <shreythakar05@gmail.com>
2026-01-04 20:47:29 +0530

    Add User_input.cpp

---
+++ Basics/User_input.cpp
@@ -0,0 +1,11 @@
+// This C++ code is to take input from the user and display it.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cin >> number;
+    cout << number;
+    return 0;
+}
