f640140665930b2d9c3c0f63267389c9e69768ad
Shrey Thakar <shreythakar05@gmail.com>
2026-01-07 21:24:48 +0530

    Create Float_multiplication.cpp

---
+++ Basics/Float_multiplication.cpp
@@ -0,0 +1,22 @@
+// This C++ code is to take two floating point numbers and print their multiplication.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float number1;
+    float number2;
+    float result;
+
+    cout << "Enter number 1: ";
+    cin >> number1;
+
+    cout << "Enter number 2: ";
+    cin >> number2;
+
+    result = number1 * number2;
+
+    cout << "Multiplication result = " << result << endl;
+
+    return 0;
+}
